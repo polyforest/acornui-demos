dc896a8a811bbc0cfa953a22294b00ee6b010fc2
Nick <nick@polyforest.com>
2019-08-28 18:43:22 -0500

    Optional composite settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,3 +24,22 @@ enableFeaturePreview("GRADLE_METADATA")
 
 // Add modules as they are created.  By default, subprojects take on the name of their root directory in gradle.
 include("text-demo", "spine-demo", "rect-demo", "datagrid-demo", "fileio-demo")
+
+// Acorn composite project as sub-projects as a workaround to https://youtrack.jetbrains.com/issue/KT-30285
+val acornUiHome: String? by extra
+
+if (acornUiHome != null && file(acornUiHome!!).exists()) {
+	include(":acornui")
+	project(":acornui").projectDir = file("$acornUiHome")
+
+	listOf("utils", "core", "game", "spine", "test-utils").forEach { acornModule ->
+		val name = ":acornui:acornui-$acornModule"
+		include(name)
+		project(name).projectDir = file("$acornUiHome/acornui-$acornModule")
+	}
+	listOf("lwjgl", "webgl").forEach { backend ->
+		val name = ":acornui:backends:acornui-$backend-backend"
+		include(name)
+		project(name).projectDir = file("$acornUiHome/backends/acornui-$backend-backend")
+	}
+}
