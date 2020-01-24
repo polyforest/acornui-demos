e4136c33715362601db8b838074e36525bbc9cf0
Nick <nick@polyforest.com>
2020-01-24 15:17:37 -0600

    Update to acornVersion 0.5.13-SNAPSHOT

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,17 @@
 rootProject.name = "acornui-demos"
 
-pluginManagement {
+buildscript {
 	val acornVersion: String by extra
 	repositories {
 		mavenLocal()
-		maven {
-			url = uri("http://artifacts.acornui.com/mvn/")
-		}
-		maven {
-			url = uri("https://dl.bintray.com/kotlin/kotlin-dev/")
-		}
-		gradlePluginPortal()
+		maven { url = uri("http://artifacts.acornui.com/mvn/") }
 	}
-	resolutionStrategy {
-		eachPlugin {
-			when {
-				requested.id.namespace == "com.acornui" ->
-					useVersion(acornVersion)
-			}
-		}
+	dependencies {
+		classpath("com.acornui:gradle-settings-plugins:$acornVersion")
 	}
 }
+
+plugins.apply("com.acornui.root-settings")
 
 // Add modules as they are created.  By default, subprojects take on the name of their root directory in gradle.
 include("text-demo", "spine-demo", "rect-demo", "datagrid-demo", "fileio-demo", "new-project", "performance-test")
